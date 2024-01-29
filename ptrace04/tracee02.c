1b2e8b1c10bdc4bbff37ee6668c23d844ee15d86
phucvin <phucvin@gmail.com>
2024-01-29 22:38:05 +0000

    tmp notes

--- ptrace04/tracee02.c
+++ ptrace04/tracee02.c
@@ -20,5 +20,14 @@ int main() {
     printf("this is %s", buf);
     write(1, buf, size);
     write(1, text, sizeof(text));
+    pause();
+    // [ptrace03] Getting write(1, 0x559250c6[2020], 37)
+    // 559250c6[0000]-559250c61000 r--p
+    // ...
+    // 559250c64000-559250c65000 rw-p
+    // --
+    // [ptrace03] Getting write(1, 0x55dad73e[a020], 37)
+    // 55dad73e[8000]-55dad73e9000 r--p
+    // 55dad73ec000-55dad73ed000 rw-p
     return 0;
 }
