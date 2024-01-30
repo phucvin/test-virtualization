f3838303454c89c4fbf89fba05278ed911f8dab6
phucvin <phucvin@gmail.com>
2024-01-30 01:47:19 +0000

    also write with data from stack

--- ptrace04/tracee02.c
+++ ptrace04/tracee02.c
@@ -6,6 +6,7 @@
 const char text[] = "this is in the program data section\n";
 
 int main() {
+    char stack_txt[] = "this is on stack\n";
     int size = 32;
     void* mem = mmap(NULL, 4096,
                      PROT_READ | PROT_WRITE,
@@ -24,5 +25,9 @@ int main() {
     write(1, buf, size);
     write(1, text, sizeof(text));
     write(1, text, sizeof(text));
+    write(1, stack_txt, sizeof(stack_txt));
+    pause();
+    // [ptrace03] Getting write(1, 0x7ffea5670480, 18)
+    // 7ffea5651000-7ffea5672000 stack
     return 0;
 }
